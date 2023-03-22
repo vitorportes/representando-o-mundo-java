2de8b3a1074ad88118442628f13e7a703176e570
vitorportes <vitorportnoy@gmail.com>
2023-03-22 18:00:50 -0300

    education class

--- src/Education.java
+++ src/Education.java
@@ -1,4 +1,14 @@
-public class Subject {
-    String name;
+public class Education {
+    String course;
     int duration;
+    String institution;
+    private double cr;
+    
+    public double getCr() {
+        return this.cr;
+    }
+
+    public void setCr(double cr) {
+        this.cr = cr;
+    }
 }
