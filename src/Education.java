6ba3fa539ee6e0db292692a12e2a7f84d4f981a6
vitorportes <vitorportnoy@gmail.com>
2023-03-22 17:49:06 -0300

    initial commit

---
+++ src/Education.java
@@ -0,0 +1,4 @@
+public class Subject {
+    String name;
+    int duration;
+}
---
+++ src/User.java
@@ -0,0 +1,23 @@
+public class User {
+    String name;
+    int age;
+    private String cpf;
+    private String email;
+    String course;
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
